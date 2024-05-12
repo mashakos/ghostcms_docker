aa12f85b239179bab6efc8bb836f1e96c7e7e24e
mashakos <mo.arabi@gmail.com>
2024-05-12 06:23:33 +0300

    Initialize claude

---
+++ Makefile
@@ -0,0 +1,68 @@
+ENVIRONMENT ?= local
+# Default image name
+IMAGE_NAME ?= ghost-remix
+# Default Ghost API key
+GHOST_CONTENT_API_KEY ?= 77a7e9c49a7cc3416ab81eb233
+BLOG_URL ?= http://localhost:8080
+SITE_TITLE ?= Test
+SITE_DESCRIPTION ?= "A test description"
+OWNER_NAME ?= Test
+OWNER_SLUG ?= test
+OWNER_EMAIL ?= test@example.com
+OWNER_PASSWORD ?= password
+JWT_SECRET ?= a543a2c76c05e0f9cbc3fa07b19ffb79af59a0b8d2d3971ac202e57853cb8cfa
+COMMENT_SETTINGS ?= all
+MAILGUN_DOMAIN ?= sandbox@mailbox.com
+MAILGUN_API_KEY ?= somkey
+MAILGUN_BASE_URL ?= https://api.mailgun.net/
+MAILGUN_USER ?= someuser
+MAILGUN_PASSWORD ?= somepassword
+LLM_URL ?= http://localhost:8081
+LLM_API_KEY ?= 68850cc7becba8a8cca282354f
+
+
+# Container name
+CONTAINER_NAME ?= ghost-remix-container
+
+# Phony targets
+.PHONY: build run all clean clean-image clean-all terminal
+
+all: build run
+clean-all: clean clean-image
+
+# Build the Docker image
+build:
+	docker build -t $(IMAGE_NAME) .
+
+# Run the Docker container
+run:
+	docker run -d --name $(CONTAINER_NAME) -p 8080:8080 \
+	-e GHOST_CONTENT_API_KEY=$(GHOST_CONTENT_API_KEY) \
+	-e SITE_TITLE=$(SITE_TITLE) \
+	-e SITE_DESCRIPTION=$(SITE_DESCRIPTION) \
+	-e OWNER_NAME=$(OWNER_NAME) \
+	-e OWNER_SLUG=$(OWNER_SLUG) \
+	-e OWNER_EMAIL=$(OWNER_EMAIL) \
+	-e OWNER_PASSWORD=$(OWNER_PASSWORD) \
+	-e ENVIRONMENT=$(ENVIRONMENT) \
+	-e BLOG_URL=$(BLOG_URL) \
+	-e JWT_SECRET=$(JWT_SECRET) \
+	-e COMMENT_SETTINGS=$(COMMENT_SETTINGS) \
+	-e MAILGUN_DOMAIN=$(MAILGUN_DOMAIN) \
+	-e MAILGUN_API_KEY=$(MAILGUN_API_KEY) \
+	-e MAILGUN_BASE_URL=$(MAILGUN_BASE_URL) \
+	-e MAILGUN_USER=$(MAILGUN_USER) \
+	-e MAILGUN_PASSWORD=$(MAILGUN_PASSWORD) \
+	-e LLM_URL=$(LLM_URL) \
+	-e LLM_API_KEY=$(LLM_API_KEY) \
+	$(IMAGE_NAME)
+
+clean:
+	docker stop $(CONTAINER_NAME) && docker rm $(CONTAINER_NAME)
+
+# Remove the Docker image
+clean-image:
+	docker rmi $(IMAGE_NAME)
+
+terminal:
+	docker exec -it $(CONTAINER_NAME) /bin/bash
---
+++ app/components/PostPage.tsx
@@ -0,0 +1,153 @@
+import { Heading, Flex, Box, Image, Button, useToast, useUpdateEffect, Spinner } from '@chakra-ui/react';
+import AuthorsList from './AuthorsList';
+import CommentsList from './CommentsList';
+import PostContent from './PostContent';
+import TopicsList from './TopicsList';
+import { useCallback, useMemo, useState } from 'react';
+import { GetPostOutput } from '~/content-api/types';
+import {
+  JsonifiedPostPageProps,
+  SummarizePostFailureResponse,
+  SummarizePostResponse,
+  SummarizePostSuccessResponse,
+} from './types';
+import { PageBase } from './PageBase';
+import { FaBolt, FaFileAlt } from 'react-icons/fa';
+import { useFetcher } from '@remix-run/react';
+
+export const PostPage = ({ post, comments, commentSettings }: JsonifiedPostPageProps): JSX.Element => {
+  const authors = post.authors ?? [];
+  const tags = post.tags ?? [];
+
+  const parseFeatureImage = (featureImageURL: string): string => {
+    if (featureImageURL.includes('__GHOST_URL__')) {
+      return featureImageURL.replace('__GHOST_URL__', '');
+    }
+
+    return featureImageURL;
+  };
+
+  const fetcher = useFetcher<SummarizePostResponse>();
+  const toast = useToast();
+
+  const [rapidRead, setRapidRead] = useState(false);
+  const [summary, setSummary] = useState<string | undefined>();
+  const [postContent, setPostContent] = useState(post.html);
+
+  const handleSummarize = useCallback((): void => {
+    if (rapidRead) {
+      setRapidRead(false);
+      setPostContent(post.html);
+      return;
+    }
+
+    if (!rapidRead && summary) {
+      setRapidRead(true);
+      setPostContent(summary);
+      return;
+    }
+
+    const form = new FormData();
+
+    form.set('post', post.html as string);
+    fetcher.formData = form;
+
+    fetcher.submit(form, {
+      method: 'POST',
+      action: `/summarize`,
+    });
+  }, [summary, rapidRead]);
+
+  useUpdateEffect(() => {
+    if ((fetcher.data as SummarizePostSuccessResponse)?.result) {
+      setSummary((fetcher.data as SummarizePostSuccessResponse).result);
+      setPostContent((fetcher.data as SummarizePostSuccessResponse).result);
+      setRapidRead(true);
+    }
+
+    if ((fetcher.data as SummarizePostFailureResponse)?.error) {
+      toast({
+        title: 'Error',
+        description: (fetcher.data as SummarizePostFailureResponse).error,
+        status: 'error',
+        duration: 9000,
+        isClosable: true,
+      });
+    }
+  }, [fetcher.data]);
+
+  const commentsOn = useMemo(() => {
+    if (post.type === 'page') return false;
+    if (commentSettings === 'off') return false;
+
+    return true;
+  }, [commentSettings, post.type]);
+
+  return (
+    <PageBase>
+      <Box pb={5} px={5}>
+        {post.feature_image && (
+          <Box
+            position="relative"
+            width="100%"
+            height="0"
+            paddingBottom={{ base: '70%', md: '40%' }}
+            overflow="hidden"
+            mt={5}
+            borderRadius="xl"
+          >
+            <Image
+              src={parseFeatureImage(post.feature_image)}
+              alt={(post as GetPostOutput).feature_image_alt ?? 'image'}
+              position="absolute"
+              top="50%"
+              left="50%"
+              transform="translate(-50%, -50%)"
+              minWidth="100%"
+              minHeight="100%"
+              objectFit="cover"
+            />
+          </Box>
+        )}
+        <Flex justifyContent="space-between" mt={{ base: 2, md: 5 }}>
+          <AuthorsList authors={authors} />
+        </Flex>
+        <Heading fontSize={{ base: 40, md: 40, lg: 50 }} textAlign={{ base: 'center', md: 'left' }} textColor="text1">
+          {post.title}
+        </Heading>
+        <Flex
+          justifyContent={tags.length > 0 ? 'space-between' : 'right'}
+          alignItems={{ base: 'left', md: 'center' }}
+          flexDirection={{ base: 'column', md: 'row' }}
+          gap={2}
+        >
+          {tags.length > 0 && <TopicsList topics={tags} />}
+          {post.type !== 'page' && (
+            <Button
+              leftIcon={rapidRead ? <FaFileAlt color="white" /> : <FaBolt color="white" />}
+              backgroundColor={rapidRead ? 'primary' : 'secondary'}
+              color="white"
+              _hover={{ bg: `${rapidRead ? 'primary' : 'secondary'}` }}
+              _active={{ bg: `${rapidRead ? 'primary' : 'secondary'}` }}
+              onClick={handleSummarize}
+            >
+              {rapidRead ? 'FullRead' : 'RapidRead'}
+            </Button>
+          )}
+        </Flex>
+      </Box>
+      <Box py={5} px={5} textColor="text1">
+        {['submitting', 'loading'].includes(fetcher.state) ? (
+          <Spinner thickness="4px" speed="0.65s" emptyColor="gray.200" color="blue.500" size="xl" />
+        ) : (
+          <PostContent html={postContent as string} />
+        )}
+      </Box>
+      {commentsOn && (
+        <Box>
+          <CommentsList comments={comments} />
+        </Box>
+      )}
+    </PageBase>
+  );
+};
